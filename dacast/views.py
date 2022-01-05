da3ca4327bb484ddcc760382786b1becee9aeee6
askar <askarjon.abdullayev@gmail.com>
2022-01-05 14:00:50 +0500

    2

--- dacast/views.py
+++ dacast/views.py
@@ -11,12 +11,18 @@ def uploadVideo(request):
         serializer = VideoSerializer(data=request.data)
         token = "1641301942hzIX4LKsvBFgHn32Ms0TuAPk1JeWBAxv"
         if serializer.is_valid():
+            file = serializer.validated_data['file']
             serializer.save()
             url = ('https://upload.dacast.com')
-            res = requests.post(url, token, serializer.data)
-            print(res.json())
+            res = requests.post(url, data={
+                'source': 'test.mp4',
+                'upload_type': 'ajax',
+                
+                'token': token,
+            })
+            # print(res.json())
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors.json())
     
     return Response("dnoe")
         
