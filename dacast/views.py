5f4c76830f069761964b7a58ade3cc53ab7bd2eb
askar <askarjon.abdullayev@gmail.com>
2022-01-05 23:27:58 +0500

    111

--- dacast/views.py
+++ dacast/views.py
@@ -1,50 +1,33 @@
-from django.http.response import HttpResponse
+from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import requests
+import json
 from dacast.serializers import VideoSerializer
-
-
-# @api_view(['GET', 'POST'])
-# def uploadVideo(request):
-#     if request.method == "POST":
-#         serializer = VideoSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             url = ('https://upload.dacast.com/v2/vod')
-#             headers = {'X-Api-Key': "to1641301942hzIX4LKsvBFgHn32Ms0TuAPk1JeWBAxvken"}
-#             res = requests.post(url, data={
-#                 'source': 'test.mp4',
-#                 'upload_type': 'ajax',
-#             },
-#             headers=headers)
-#             print(res)
-#         else:
-#             return Response(serializer.errors.json())
-    
-#     return Response("dnoe")
         
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def getToken(request):
-    apikey = "to1641301942hzIX4LKsvBFgHn32Ms0TuAPk1JeWBAxvken"
-    tokenUrl = "https://developer.dacast.com/?apikey=" + apikey
-    callbackUrl = 'https://127.0.0.1:8000'
-    uploadUrl = "https://upload.dacast.com"
+    apikey = "1641301942hzIX4LKsvBFgHn32Ms0TuAPk1JeWBAxv"
+    tokenUrl = f"https://developer.dacast.com/v2/vod/"
+    uploadUrl = "https://upload.dacast.com/v2/vod"
     if request.method == "POST":
         print("getting token")
-        
         serializer = VideoSerializer(data=request.data)
         if serializer.is_valid():
-            FormData = {
-                'apikey'
-                'source': serializer.data['file'],
-                'callbackUrl': callbackUrl,
-                'upload_type': 'ajax',
+            data = {
+                "source": serializer.validated_data['file'],
+                "upload_type": "ajax",
             }
-            resTok = requests.post(tokenUrl)
-            res = requests.post(uploadUrl, FormData, headers=resTok)
-            print(res)
+            headers = {
+                "X-Api-Key": apikey,
+            }
+            resTok = requests.post(tokenUrl, headers=headers, data=data)
+            print(data)
+            print("token: ", resTok)
+            res = requests.post(
+                uploadUrl, data, headers=resTok)
+            print("upload: ", res)
         else:
             return Response(serializer.errors.json())
 
